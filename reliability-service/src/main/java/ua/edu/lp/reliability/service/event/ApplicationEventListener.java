fa8b4cffa050bd5b9ef559625001355caa790bc7
Iurii_Parfeniuk <iurii.parfeniuk@gmail.com>
2013-11-24 22:55:03 +0200

    Updated Event service

--- reliability-service/src/main/java/ua/edu/lp/reliability/service/event/ApplicationEventListener.java
+++ reliability-service/src/main/java/ua/edu/lp/reliability/service/event/ApplicationEventListener.java
@@ -2,7 +2,7 @@ package ua.edu.lp.reliability.service.event;
 
 import ua.edu.lp.reliability.model.event.ApplicationEvent;
 
-public interface ApplicationEventListener<EVENT extends ApplicationEvent> {
+public interface ApplicationEventListener<EVENT extends ApplicationEvent<?>> {
 
 	void handle(EVENT event);
 }
